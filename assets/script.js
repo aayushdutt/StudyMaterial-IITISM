7a8cd3a90ae0ad771d87d6f56d18e42e66d86758
Aayush Dutt <aayushd100@gmail.com>
2019-02-09 18:55:12 +0530

    add data grab and fetch logic, add table, add tablesort to sort table optimise styling, add form and other links

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,19 @@ const URL = "https://api.sheety.co/4d6ce4fa-451d-4b6f-b4fb-55c7fffd999c";
 const inputs = document.querySelectorAll(".control");
 const submitButton = document.querySelector("button.button");
 
+submitButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  var inputValues = getInputValues();
+
+  var apiData;
+  fetch(URL)
+    .then(data => data.json())
+    .then(fetchedData => {
+      apiData = fetchedData;
+      getMatchedData(inputValues, apiData);
+    });
+});
+
 function getInputValues() {
   var inputValues = {
     branch: inputs[0].children[0].value,
@@ -13,14 +26,19 @@ function getInputValues() {
   return inputValues;
 }
 
-submitButton.addEventListener("click", function(e) {
-  e.preventDefault();
-  var inputValues = getInputValues();
+function getMatchedData(inputData, apiData) {
+  console.log(inputData, apiData);
+  var { branch, year, semester } = inputData;
+  var matchedData = [];
+  apiData.forEach(paper => {
+    if (
+      paper.branch === branch &&
+      paper.year == year &&
+      paper.semester == semester
+    )
+      matchedData.push(paper);
+  });
 
-  var apiData;
-  fetch(URL)
-    .then(data => data.json())
-    .then(fetchedData => {
-      apiData = fetchedData;
-    });
-});
+  console.log(matchedData);
+}
+new Tablesort(document.getElementById("table-id"));
